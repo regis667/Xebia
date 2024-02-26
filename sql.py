f8892771519c4fe11b9caf81c2df1b98f9883983
Dominik Weremiuk <dominik.weremiuk@brenntag-external.com>
2024-02-26 16:42:54 +0100

    sql

--- sql.py
+++ sql.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python
-import MySQLdb
+import mysql.connector
+from flask import Flask
 
-db = MySQLdb.connect(host="dwrds.cgpzlgzs9ybi.eu-central-1.rds.amazonaws.com",    # your host, usually localhost
+
+db = mysql.connector.connect(host="dwrds.cgpzlgzs9ybi.eu-central-1.rds.amazonaws.com",    # your host, usually localhost
                      user="dw",         # your username
                      passwd="12345678",  # your password
-                     db="dw")        # name of the data base
+                     db="mydb")        # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor()
+#sqldel = "DROP TABLE dominiks"
+
+#cur.execute(sqldel)
+
+#cur.execute("CREATE DATABASE dwdb")
+
+#cur.execute("CREATE TABLE dominiks (name VARCHAR(255), address VARCHAR(255))")
+sql = "INSERT INTO dominiks (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+cur.executemany(sql, val)
 
 # Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
-
+cur.execute("SELECT * FROM dominiks")
+myresult = cur.fetchall()
 # print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
-
-db.close()
+#for row in cur.fetchall():
+#    print ("row[0]")
+for x in myresult:
+      print(x)
+db.close()0
