14588c8e5c6623763ac98879078d791f188066ef
Dominik Weremiuk <dominik.weremiuk@brenntag-external.com>
2024-02-27 13:38:17 +0100

    init.sh added

--- init.sh
+++ init.sh
@@ -3,5 +3,11 @@ sudo yum install httpd -y
 sudo yum install git -y
 sudo yum install ec2-instance-connect
 systemctl enable httpd
-git clone https://github.com/florient2016/myweb.git /var/www/html/web/
 systemctl start httpd
+yum install python -y
+git clone https://github.com/regis667/Xebia.git
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+pip install mysql-connector-python
+pip install flask
+python sql.py
