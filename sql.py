6e38e76cd31001327d8fde47b8efd4253216ad9b
Dominik Weremiuk <dominik.weremiuk@brenntag-external.com>
2024-02-28 14:57:18 +0100

    bastion init

--- sql.py
+++ sql.py
@@ -18,24 +18,6 @@ def add_print_sql():
     #cur.execute("CREATE DATABASE dwdb")
 
     #cur.execute("CREATE TABLE dominiks (name VARCHAR(255), address VARCHAR(255))")
-    sql = "INSERT INTO dominiks (name, address) VALUES (%s, %s)"
-    val = [
-      ('Peter', 'Lowstreet 4'),
-      ('Amy', 'Apple st 652'),
-      ('Hannah', 'Mountain 21'),
-      ('Michael', 'Valley 345'),
-      ('Sandy', 'Ocean blvd 2'),
-      ('Betty', 'Green Grass 1'),
-      ('Richard', 'Sky st 331'),
-      ('Susan', 'One way 98'),
-      ('Vicky', 'Yellow Garden 2'),
-      ('Ben', 'Park Lane 38'),
-      ('William', 'Central st 954'),
-      ('Chuck', 'Main Road 989'),
-      ('Viola', 'Sideway 1633')
-    ]
-    cur.executemany(sql, val)
-
     # Use all the SQL you like
     cur.execute("SELECT * FROM dominiks")
     list=[]
@@ -54,6 +36,5 @@ app = Flask(__name__)
 @app.route('/')
 def home ():
     a=add_print_sql()
-    return a, 200
-app.debug=True
+    return a
 app.run()
