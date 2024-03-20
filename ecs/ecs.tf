53709e5aa29295d77be2162f0bebdb10c5628291
Dominik Weremiuk <dominik.weremiuk@brenntag-external.com>
2024-03-20 16:22:58 +0100

    task def

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -425,3 +425,42 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
    }
  ])
 }
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = [for subnet in aws_subnet.dw-public-ecs : subnet.id]
+   security_groups = [aws_security_group.web_sg.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ triggers = {
+   redeployment = timestamp()
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.target.arn
+   container_name   = "docker-ecs"
+   container_port   = 80
+ }
+load_balancer {
+   target_group_arn = aws_lb_target_group.targets3.arn
+   container_name   = "docker-ecs"
+   container_port   = 4000
+ }
+ depends_on = [aws_autoscaling_group.ecs_asg]
+}
+
